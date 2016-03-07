f424c1d5c2025002d24702105fb05f70c8f4a244
Kenneth Hanson <khanson679@gmail.com>
2016-03-08 00:11:19 +0900

    Added tests.
    One test currently fails, because of missing entries in Hiragana table.

--- test.py
+++ test.py
@@ -7,7 +7,24 @@ import unittest
 from romajitools import *
 
 class RTTestCase(unittest.TestCase):
+    def test_lemma_tables(self):
+        # sanity test
+        self.assertEqual(len(LEMMAS), 233)
+        
+        # have all lemmas in the full table been categorized?
+        self.assertEqual(
+            len(LEMMAS),
+            len(LEMMAS_BASIC) + len(LEMMAS_EXTENDED) + len(LEMMAS_EXTRA))
+    
+    def test_hiragana_table(self):
+        # check that all Hiragana entries have a lemma, and vis versa
+        for lemma in FROM_HIRA.itervalues():
+            self.assertTrue(lemma in LEMMAS)
+        for lemma in LEMMAS:
+            self.assertTrue(lemma in FROM_HIRA.itervalues())
+        
     def test_convert_from_hira(self):
+        # sanity test
         self.assertEqual(to_wapuro("ひらがな"), "hiragana")
 
 if __name__ == '__main__':
