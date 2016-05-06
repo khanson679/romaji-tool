e1d23c98c38a9c7a7d11a8f49a7bd2651b821e7c
Kenneth Hanson <khanson679@gmail.com>
2016-05-06 22:10:38 +0900

    Refactored core logic into Mapping, wrapped by TextFormat and RomajiFormat.

--- romajitools/mapping.py
+++ romajitools/mapping.py
@@ -4,20 +4,106 @@ from __future__ import unicode_literals
 
 import re
 
-import defs
+import util
 
 
 class Mapping(object):
     """
-    Defines a partial mapping from the internal representation to a surface form.
-
-    A mapping can be one-to-one, one-to-many, or many-to-one.
+    Defines mapping from a surface string to the internal representation.
     """
 
-    def __init__(self, mapping, direction="both", *addons):
+    def __init__(self, base_map=None, in_map=None, out_map=None):
         """
-        mapping -- a list of tuples of the form surface:underlying
-        addon   -- additional 
-        context -- a regex lookahead/lookbehind string
+        base_map -- a dict containing a bidirectional mapping of surface:internal pairs
+        in_map -- a dict containing a unidirectional mapping from surface to internal rep
+        out_map -- a dict containing a unidirectional mapping to surface from internal rep
+
+        Contents of `in_map` and `out_map` override `base_map`.
         """
-        pass
+        if base_map is None:
+            base_map = {}
+        if in_map is None:
+            in_map = {}
+        if out_map is None:
+            out_map = {}
+
+        inverse_base_map = {lemma:text for text, lemma in base_map.iteritems()}
+
+        self._surface_to_underlying = dict(base_map, **in_map)
+        self._underlying_to_surface = dict(inverse_base_map, **out_map)
+            
+        self._parse_pattern = re.compile(
+            "$" + \
+            "|".join(sorted(self._surface_to_underlying.keys(), key=len, reverse=True)) + \
+            "^")
+        self._emit_pattern = re.compile(
+            "$" + \
+            "|".join(sorted(self._underlying_to_surface.keys(), key=len, reverse=True)) + \
+            "^")
+
+    def __unicode__(self):
+        return (
+            "{}\n"
+            "---------------\n"
+            "{}\n"
+            "\n"
+            "{}\n"
+            .format(
+                unicode(self._name),
+                ",  ".join(" ".join(pair) for pair in self._surface_to_underlying.iteritems()),
+                ",  ".join(" ".join(pair) for pair in self._underlying_to_surface.iteritems()))
+            )
+    
+    def accepted_surface_substrings(self):
+        """
+        Returns iterator over list of keys in mapping from format to internal rep.
+        """
+        return self._surface_to_underlying.iterkeys()
+
+    def accepted_internal_substrings(self):
+        """
+        Returns iterator over list of keys in mapping to format from internal rep.
+        """
+        return self._underlying_to_surface.iterkeys()
+
+    def produced_surface_substrings(self):
+        """
+        Returns iterator over list of values in mapping to surface from internal rep.
+        """
+        return self._underlying_to_surface.itervalues()
+
+    def produced_internal_substrings(self):
+        """
+        Returns iterator over list of values in mapping from surface to internal rep.
+        """
+        return self._surface_to_underlying.itervalues()
+
+    def match_surface(self, string):
+        """
+        Returns True if entire string can be matched by surface format, False otherwise.
+        """
+        return re.match(pattern=self._parse_pattern,
+                        string=string)
+
+    def match_underlying(self, string):
+        """
+        Returns True if entire string can be matched by internal format, False otherwise.
+        """
+        return re.match(pattern=self._emit_pattern,
+                        string=string)
+
+    def parse(self, string):
+        """
+        Return a string (partially) converted to the internal representation.
+        """
+        return re.sub(pattern=self._parse_pattern,
+                      repl=lambda x: self._surface_to_underlying[x.group(0)],
+                      string=string)
+
+    def emit(self, string):
+        """
+        Return a string (partially) converted to surface representation.
+        """
+        return re.sub(pattern=self._emit_pattern,
+                      repl=lambda x: self._underlying_to_surface[x.group(0)],
+                      string=string)
--- romajitools/textformat.py
+++ romajitools/textformat.py
@@ -5,112 +5,115 @@ from __future__ import unicode_literals
 import re
 
 import defs
-
+import mapping
 
 class TextFormat(object):
     """
     Defines mapping from a Kana or romanization format to the internal representation.
     """
 
-    def __init__(self, name, base_map, from_map=None, to_map=None):
+    def __init__(self, name, mapping):
         """
-        name     -- the name of the format
-        base_map -- a dict containing bidirectional mapping, as text:lemma pairs
-        from_map -- a dict containing unidirectional mapping from this format to internal rep
-        to_map   -- a dict containing unidirectional mapping to this format from internal rep
-
-        Contents of `from_map` and `to_map` override `base_map`.
+        name    -- the name of the format
+        mapping -- a mapping from this format to the underlying representation
         """
-        if from_map is None:
-            from_map = {}
-        if to_map is None:
-            to_map = {}
-
-        inverse_base_map = {lemma:text for text, lemma in base_map.iteritems()}
-
         self._name = name
-        self._moras_to_lemmas = dict(base_map, **from_map)
-        self._lemmas_to_moras = dict(inverse_base_map, **to_map)
-        
-        self._parse_pattern = re.compile(
-            "|".join(sorted(self._moras_to_lemmas.keys(), key=len, reverse=True)))
-        self._emit_pattern = re.compile(
-            "|".join(sorted(self._lemmas_to_moras.keys(), key=len, reverse=True)))
+        self._mapping = mapping
 
     def __unicode__(self):
         return (
             "{}\n"
             "---------------\n"
             "{}\n"
-            "\n"
-            "{}\n"
-            .format(
-                unicode(self._name),
-                ",  ".join(" ".join(pair) for pair in self._moras_to_lemmas.iteritems()),
-                ",  ".join(" ".join(pair) for pair in self._lemmas_to_moras.iteritems()))
-            )
+            .format(unicode(self._name), unicode(self.mapping))
+        )
 
     @property
     def name(self):
         return self._name
     
-    def accepted_moras(self):
-        """
-        Returns iterator over list of keys in mapping from format to internal rep.
-        """
-        return self._moras_to_lemmas.iterkeys()
-
-
     def accepted_lemmas(self):
-        """
-        Returns iterator over list of keys in mapping to format from internal rep.
-        """
-        return self._lemmas_to_moras.iterkeys()
-
-
-    def produced_moras(self):
-        """
-        Returns iterator over list of values in mapping to format from internal rep.
-        """
-        return self._lemmas_to_moras.itervalues()
-
+        return self._mapping.accepted_internal_substrings()
 
     def produced_lemmas(self):
-        """
-        Returns iterator over list of values in mapping from format to internal rep.
-        """
-        return self._moras_to_lemmas.itervalues()
-
-
-    def match(self, string):
-        """
-        Returns True if string matches format, False otherwise.
-        """
-        return re.match(pattern=self._parse_pattern,
-                        string=string)
-
+        return self._mapping.produced_internal_substrings()
 
     def parse(self, string):
         """
         Return a string converted to the internal representation.
         """
-        return re.sub(pattern=self._parse_pattern,
-                      repl=lambda x: self._moras_to_lemmas[x.group(0)],
-                      string=string)
-
+        return self._mapping.parse(string)
 
     def emit(self, string):
         """
         Return a string converted to this format.
         """
-        return re.sub(pattern=self._emit_pattern,
-                      repl=lambda x: self._lemmas_to_moras[x.group(0)],
-                      string=string)
+        return self._mapping.emit(string)
+
+
+class RomajiFormat(TextFormat):
+    """
+    Defines a mapping from a romanization format to the internal representation.
+
+    Handles sokuon, etc.
+    """
+
+    def __init__(self, name, base, nasal, sokuon, chouon):
+        """
+        Build format from the given lists of Mappings.
+
+        Moras that are not handled will be unchanded during conversion.
+        
+        base -- base moras and you-on
+        nasal -- nasal mora with preceding consonants
+        sokuon -- sokuon moras with proceding consonants
+        chouon -- chou-on mark with preceding vowels
+        """
+        self._name = name
+        self._base_map = base
+        self._nasal_map = nasal
+        self._sokuon_map = sokuon
+        self._chouon_map = chouon
+
+    def accepted_lemmas(self):
+        return self._base_map.accepted_internal_substrings()
+
+    def produced_lemmas(self):
+        return self._base_map.produced_internal_substrings()
+
+    def parse(self, string):
+        """
+        Return a string converted to the internal representation.
+        """
+        out = string
+        for mapping in (self._chouon_map,
+                        self._sokuon_map,
+                        self._nasal_map,
+                        self._base_map):
+            out = mapping.parse(string)
+        return out
+
+    def emit(self, string):
+        """
+        Return a string converted to this format.
+        """
+        out = string
+        for mapping in (self._base_map,
+                        self._nasal_map,
+                        self._sokuon_map,
+                        self._chouon_map):
+            out = mapping.emit(string)
+        return out
 
 
 #
 # init text formats
 #
 
-HIRAGANA = TextFormat("Hiragana", defs.HIRAGANA_TAB)
-WAPURO = TextFormat("Wapuro", defs.WAPURO_TAB)
+WAPURO   = TextFormat("Wapuro", mapping.Mapping())
+HIRAGANA = TextFormat("Hiragana", mapping.Mapping(defs.HIRAGANA_TAB))
+NIHON    = RomajiFormat("Nihon",
+                        mapping.Mapping(defs.MORAS_NIHON),
+                        mapping.Mapping(defs.NASAL_BASE),
+                        mapping.Mapping(defs.SOKUON_BASE),
+                        mapping.Mapping(defs.CHOUON_DOUBLE_VOWEL))
